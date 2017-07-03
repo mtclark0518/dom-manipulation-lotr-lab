34b681e64847f2dfe8769b62e521b41b5c6c533e
mtclark0518 <mtclark0518@gmail.com>
2017-07-02 20:44:22 -0600

    there and back again bitches

--- starter-code/app/scripts/fellowship.js
+++ starter-code/app/scripts/fellowship.js
@@ -24,9 +24,6 @@ var Gandalf;
 var Frodo;
 var theOneRing;
 
-
-
-
 // Part 1
 var middleEarth = document.createElement('section');
 middleEarth.setAttribute('id', 'middle-earth');
@@ -50,8 +47,6 @@ function makeMiddleEarth() {
 makeMiddleEarth();
 
 //var middleEarth = document.getElementById('middleEarth');
-
-
 
 // Part 2
 var theShire = document.querySelector('article');
@@ -92,7 +87,6 @@ function keepItSecretKeepItSafe() {
 }
 keepItSecretKeepItSafe();
 
-
 // Part 4
 var crew = document.createElement('ul');
 var Rivendell = middleEarth.firstChild.nextSibling;
@@ -113,7 +107,6 @@ function makeBuddies() {
 }
 makeBuddies();
 
-
 // Part 5
 var whoseInMyCrew = crew.getElementsByTagName('li');
 var Aragorn = document.createElement('li');
@@ -129,7 +122,6 @@ function beautifulStranger() {
   }
 beautifulStranger();
 
-
 // Part 6
 function leaveTheShire() {  
   theShire.removeChild(hobbitSquad); //gather the hobbits
@@ -139,28 +131,25 @@ function leaveTheShire() {
   }
 leaveTheShire();
 
-
 // Part 7
-var theFellowship = document.createElement('ul');
+var theFellowship = document.createElement('div');
+theFellowship.setAttribute('id', 'the-fellowship');
 var demHobbits  = hobbitSquad.getElementsByTagName('li');
 function forgeTheFellowShip() {
   // create a new div called 'the-fellowship' within rivendell
-  var the_fellowship = document.createElement('div');
-  the_fellowship.setAttribute('id', 'the-fellowship');
-  the_fellowship.appendChild(theFellowship);
-  Rivendell.appendChild(the_fellowship);
+  Rivendell.appendChild(theFellowship);
 
   //add each hobbit
   for(var j = 0; j < demHobbits.length; j++){
     var cloneDemHobbits = demHobbits[j].cloneNode(true);
     theFellowship.appendChild(cloneDemHobbits);
-   // alert(demHobbits[j].innerText + " has joined the fellowship");
+    alert(demHobbits[j].innerText + " has joined the fellowship");
   }
   // add each buddy 
   for(var i = 0; i < whoseInMyCrew.length; i++){
     var cloneCrew = whoseInMyCrew[i].cloneNode(true);
     theFellowship.appendChild(cloneCrew);
-    //alert(whoseInMyCrew[i].innerText + " has joined the fellowship");
+    alert(whoseInMyCrew[i].innerText + " has joined the fellowship");
   }
   Rivendell.removeChild(hobbitSquad);
   Rivendell.firstChild.nextSibling.removeChild(crew);
@@ -174,12 +163,12 @@ for(var i = 0; i < theFellowshipOfTheRing.length; i++){
     theFellowshipOfTheRing[i].setAttribute('data-id', i);
 // change the 'Gandalf' textNode to 'Gandalf the White'        
     if(theFellowshipOfTheRing[i].innerHTML === 'Gandalf the Grey'){
-        var Gandalf = theFellowshipOfTheRing[i];
-        Gandalf.innerHTML = 'Gandalf the White';
+      var Gandalf = theFellowshipOfTheRing[i];
+      Gandalf.innerHTML = 'Gandalf the White';
 // apply style to the element
 // make the background 'white', add a grey border
-        Gandalf.style.background = 'white';
-        Gandalf.style.border = '4px solid grey';
+      Gandalf.style.background = 'white';
+      Gandalf.style.border = '4px solid grey';
     }
   }
 }
@@ -189,33 +178,65 @@ theBalrog();
 
 function hornOfGondor() {
   // pop up an alert that the horn of gondor has been blown
+  alert('the horn of gondor has been blown.');
   // Boromir's been killed by the Uruk-hai!
   // put a linethrough on boromir's name
+  theFellowshipOfTheRing[8].setAttribute('style', 'text-decoration: line-through;');
   // Remove Boromir from the Fellowship
+  theFellowship.removeChild(theFellowshipOfTheRing[8]);
 }
+hornOfGondor();
 
 
 // Part 10
-
+var Mordor = middleEarth.lastChild;
+var mountDoom = document.createElement('div');
+mountDoom.setAttribute('id', 'mount-doom');
 function itsDangerousToGoAlone(){
-  // take Frodo and Sam out of the fellowship and move them to Mordor
   // add a div with an id of 'mount-doom' to Mordor
+  // take Frodo and Sam out of the fellowship and move them to Mordor 
+  var Frodo = theFellowshipOfTheRing[0];
+  var Samwise = theFellowshipOfTheRing[1];
+  theFellowship.removeChild(Frodo);
+  theFellowship.removeChild(Samwise);
+  Mordor.appendChild(Frodo);
+  Mordor.appendChild(Samwise);
+  Mordor.appendChild(mountDoom);
 }
-
+itsDangerousToGoAlone();
 
 // Part 11
-
+var gollum = document.createElement('div');
+gollum.setAttribute('id', 'gollum');
 function weWantsIt() {
+  var Frodo = Mordor.querySelector('li'); 
+  var thePrecious = Mordor.querySelector('div#the-ring'); 
   // Create a div with an id of 'gollum' and add it to Mordor
+  Mordor.appendChild(gollum);
   // Remove the ring from Frodo and give it to Gollum
+  Frodo.removeChild(thePrecious);
+  gollum.appendChild(thePrecious);
   // Move Gollum into Mount Doom
+  Mordor.removeChild(gollum);
+  mountDoom.appendChild(gollum);
 }
-
+weWantsIt();
 
 // Part 12
-
+var crispyHobbits = Mordor.getElementsByTagName('li');
 function thereAndBackAgain() {
   // remove Gollum and the Ring from the document
+  mountDoom.removeChild(gollum);
+  function returnOfTheEagles(){
+    Mordor.removeChild(crispyHobbits[0]);
+  }
+  returnOfTheEagles();
+  returnOfTheEagles();
   // remove all the baddies from the document
+  Rivendell.removeChild(theFellowship);
+  console.log(Rivendell);
   // Move all the hobbits back to the shire
+  theShire.appendChild(hobbitSquad);
+  Frodo.removeChild(theOneRing);
 }
+thereAndBackAgain();
